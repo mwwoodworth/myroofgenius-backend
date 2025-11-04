2d776e79d429b4144d0dc794635c1b23e5b4d246
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-11-04 13:28:53 -0700

    cdxwcpc

--- scripts/sql/CHECK_USERS_COLUMNS.sql
+++
@@ -1,5 +0,0 @@
--- Check columns in users table
-\d users
-
--- Also check if any of our test users exist
-SELECT id, email FROM users WHERE email IN ('test@brainops.com', 'admin@brainops.com', 'demo@myroofgenius.com');
